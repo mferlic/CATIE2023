e03a261d69c6fd25f3b081f4b7f963f60be2fab7
Mason Ferlic <77696671+mferlic@users.noreply.github.com>
2023-02-01 10:14:33 -0500

    deploy

--- R/data-doc.R
+++ R/data-doc.R
@@ -31,10 +31,19 @@
 #' Dataset created for CATIE 2023
 #'
 #' @format A data frame with 200 rows and 11 columns
-#'
-#' #' \describe{
+#'#' \describe{
 #'    \item{ID}{numeric id}
+#'    \item{O11}{baseline covariate; range 0 -- 80}
+#'    \item{O12}{baseline covariate; range 0 -- 50}
+#'    \item{A1}{stage 1 treatment assignment. A1 = -1 if the first stage intervention included SGD, A2 = 1 if the first stage intervention did not include SGD}
+#'    \item{R}{response status. R = 0 if participant did not respond to the first stage intervention; R = 1 if he or she responded.}
+#'    \item{O21}{time-varying covariate; continuous}
+#'    \item{O22}{time-varying covariate; continuous}
+#'    \item{Y1}{score at the end of stage 1}
+#'    \item{A2}{stage 2 treatment assignment. Only non-responders (R=0) to  A1 = 1 entered the second stage of intervention and were re-randomized to either ADDING SGD (coded as -1) or INTENSIFYING the first stage intervention (coded as 1). For all others, A2 = NA.}
+#'    \item{Y2}{end-of study score}
 #' }
+#' @inheritSection simAUTISMsmart Operating Characteristics
 #'
 #' @source NULL
 "autism"
