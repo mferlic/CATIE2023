383441620611cbf348bcd30b8a645b5cc9433683
Mason Ferlic <77696671+mferlic@users.noreply.github.com>
2023-02-01 11:12:28 -0500

    added DTR means

--- data-raw/adhd.R
+++ data-raw/adhd.R
@@ -3,5 +3,9 @@
 set.seed(2023-01-01)
 adhdObj <- simADHDsmart()
 adhd <- adhdObj$data
+adhdDTRmeans <- adhdObj$DTRmeans
+
 readr::write_csv(adhd, here::here("data-raw","adhd-simulated-2023.csv"))
+
 usethis::use_data(adhd, overwrite = TRUE)
+usethis::use_data(adhdDTRmeans, overwrite = TRUE)
--- data-raw/autism.R
+++ data-raw/autism.R
@@ -3,5 +3,8 @@
 set.seed(2023-01-21)
 autObj <- simAUTISMsmart()
 autism <- autObj$data
+autismDTRmeans <- autObj$DTRmeans
 readr::write_csv(autism, here::here("data-raw","autism-simulated-2023.csv"))
+
 usethis::use_data(autism, overwrite = TRUE)
+usethis::use_data(autismDTRmeans, overwrite = TRUE)
